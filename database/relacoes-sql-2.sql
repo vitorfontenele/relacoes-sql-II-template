e0a95028a4dc4ea3431d96fc6fb4c86965deb857
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-01-19 11:26:36 -0300

    Update exercise

--- database/relacoes-sql-2.sql
+++ database/relacoes-sql-2.sql
@@ -1,2 +1,56 @@
 
 -- Práticas
+CREATE TABLE users (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    created_at TEXT DEFAULT(DATETIME('now', 'localtime')) NOT NULL
+);
+
+INSERT INTO users(id, name, email, password)
+VALUES
+("U001", "Indio Medeiros", "indiom@gmail.com", "123456"),
+("U002", "Clara Meirelles", "claram@gmail.com", "qwerty"),
+("U003", "Luiz Barbosa", "luizb@gmail.com", "password");
+
+SELECT * FROM users;
+
+CREATE TABLE follows(
+    follower_id TEXT NOT NULL,
+    followed_id TEXT NOT NULL,
+    FOREIGN KEY (follower_id) REFERENCES users(id)
+);
+
+INSERT INTO follows (follower_id, followed_id) VALUES
+    ("U001", "U002"),
+    ("U001", "U003"),
+    ("U002", "U001");
+
+SELECT * FROM users;
+
+SELECT * FROM users
+INNER JOIN follows
+ON users.id = follows.follower_id;
+
+SELECT * FROM users
+LEFT JOIN follows
+ON users.id = follows.follower_id
+INNER JOIN users AS usersFollowed
+ON usersFollowed.id = follows.followed_id;
+
+SELECT * FROM users
+LEFT JOIN follows
+ON follows.follower_id = users.id;
+
+SELECT
+users.id AS userId,
+users.name,
+users.email,
+users.password,
+usersFollowed.name as userFollowed
+FROM users
+LEFT JOIN follows
+ON follows.follower_id = users.id
+LEFT JOIN users AS usersFollowed
+ON follows.followed_id = usersFollowed.id;
